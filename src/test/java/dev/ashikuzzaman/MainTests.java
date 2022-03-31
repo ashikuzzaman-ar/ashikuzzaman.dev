51ace225fdb8cc85be7eb7435e2fe8024689f1e2
Ashikuzzaman <ashikuzzaman.ar@gmail.com>
2022-03-31 23:54:49 +0600

    Initialize empty spring boot project
    Signed-off-by: Ashikuzzaman <ashikuzzaman.ar@gmail.com>

---
+++ src/test/java/dev/ashikuzzaman/MainTests.java
@@ -0,0 +1,16 @@
+package dev.ashikuzzaman;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class MainTests {
+
+	@Test
+	public void contextLoads() {
+		assertTrue(true);
+	}
+
+}
